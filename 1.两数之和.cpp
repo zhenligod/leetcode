ce39d4b796fadde3fd960335f73d455dbe295534
morpheuslv <morpheuslv@tencent.com>
2020-02-06 12:11:26 +0800

    feat : add two sum solution

---
+++ 1.两数之和.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+#include<vector>
+#include<map>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> record;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            int tmp=target-nums[i];
+            if(record.find(tmp)!=record.end()){
+                res.push_back(i);
+                res.push_back(record[tmp]);
+                break;
+            }else
+                record[nums[i]]=i;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
